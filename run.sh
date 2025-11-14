c2d3d8be6765c4e5a11b0d3c1ab3789b69cb149e
navrkalpavel <48756661+navrkalpavel@users.noreply.github.com>
2025-11-14 10:32:31 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 set -e
 
-KEY=/data/server_key
-
-if [ ! -f "$KEY" ]; then
-    echo "[upterm] Generating SSH host key"
-    ssh-keygen -t ed25519 -N "" -f "$KEY"
-fi
+export XDG_CONFIG_HOME="/data/upterm"
+mkdir -p /data/upterm
 
 echo "[upterm] Starting uptermd on port 22"
 
 /usr/local/bin/uptermd \
-  serve \
-  --ssh-host-key "$KEY" \
-  --bind-addr 0.0.0.0:22
+  --ssh-addr 0.0.0.0:22
